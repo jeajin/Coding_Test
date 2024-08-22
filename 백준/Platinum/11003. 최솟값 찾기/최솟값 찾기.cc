2ec708ec0b30b2912fa8684a5b2d377a3655ec2e
jaejin <78140235+jeajin@users.noreply.github.com>
2024-08-23 03:54:21 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 1136 ms, Memory: 62996 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
@@ -17,11 +17,11 @@ int main() {
     int N, L;
     cin >> N >> L;
     int arr[N];
-    for (int i = 0; i < N; ++i)
+    for (int i = 0; i < N; i++)
         cin >> arr[i];
 
     deque<A> dq;
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < N; i++) {
     	while (!dq.empty() && dq.back().num > arr[i])
             dq.pop_back();
  		dq.push_back({ arr[i], i });   	
