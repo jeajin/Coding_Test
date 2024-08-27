7b9979ac133865ee35c4369c4f5312f839e2bc17
jaejin <78140235+jeajin@users.noreply.github.com>
2024-08-28 01:44:25 +0900

    [Silver V] Title: K번째 수, Time: 636 ms, Memory: 21552 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.cc
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+
 using namespace std;
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL); 
@@ -9,10 +12,10 @@ int main() {
     int N,in;
     cin >> N >> in;
     
-	int arr[N];
+	vector<int> arr(N);
 	
 	for(int i=0;i<N;i++){cin >> arr[i];}
-	sort(arr,arr+N);
+	nth_element(arr.begin(), arr.begin() + in - 1, arr.end());
 	
 	printf("%d",arr[in-1]);
 }
