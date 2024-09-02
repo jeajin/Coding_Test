0a5ebd2ccf26660f46e31f56a19b32a0b1271d5c
jaejin <78140235+jeajin@users.noreply.github.com>
2024-09-02 13:51:42 +0900

    [Gold V] Title: 신기한 소수, Time: 304 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.cc
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.cc
@@ -12,18 +12,17 @@ bool isprime(int num){
 	}
 	return true;
 }
-bool dfs(int num,int depth){
-	
-	if(!isprime(num)) return 0;
+bool dfs(int num,int depth){	
+
+	if(d==depth) printf("%d\n",num);
 	else{
-		if(d==depth) printf("%d\n",num);
-		else{
-			depth++;
-			for(int i=1;i<10;i+=2){
-				dfs(num*10+i,depth);
-			}
+		depth++;
+		for(int i=1;i<10;i+=2){
+			if(isprime(num*10+i))
+			dfs(num*10+i,depth);
 		}
 	}
+	
 	
 	return 1;
 
@@ -40,6 +39,4 @@ int main() {
 	dfs(3,1);
 	dfs(5,1);
 	dfs(7,1);
-	
-
 }
