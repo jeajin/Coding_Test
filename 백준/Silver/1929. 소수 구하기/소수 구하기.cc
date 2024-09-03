746ebf0c38307ac052faf281d1b6410c1d1b9837
jaejin <78140235+jeajin@users.noreply.github.com>
2024-09-03 18:48:41 +0900

    [Silver III] Title: 소수 구하기, Time: 12 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -4,14 +4,6 @@ using namespace std;
 int prime[1000001];
 int start,endN;
 
-bool is_prime(int num){
-	for(int i=2;i*i<=num;i++){
-		if(num%i==0){
-			return 0;
-		}
-	}
-	return 1;
-}
 void set_prime_multi_zero(int num){
 	int mul=num;
 	while((mul+=num)<=endN){
@@ -26,8 +18,8 @@ int main() {
 	fill_n(prime, endN+1, 1); 
  	prime[1]=0;
  	set_prime_multi_zero(2);
-	for(int i=3;i<=endN;){
-		if(is_prime(i)){
+	for(int i=2;i*i<=endN;){
+		if(prime[i]){
 			set_prime_multi_zero(i);
 		}
 		while(prime[++i]==0){
