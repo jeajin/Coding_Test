1ead24626e6cb10659a059132de0b99d1bf582a9
jaejin <78140235+jeajin@users.noreply.github.com>
2024-09-04 00:15:47 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 4 ms, Memory: 3984 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.cc
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.cc
@@ -3,6 +3,7 @@
 using namespace std;
 #define max 1000001
 bool nn[max];
+bool prime[max];
 
 long long start,endN;
 long long mul,temp,tt,ttt;
@@ -14,9 +15,15 @@ int main() {
 	cin >>  start >> endN;
 	
 	fill_n(nn, max, 1); 	
+	fill_n(prime, max, 1); 
+	prime[1]=0;
     long long s2 = sqrt(endN);
     
 	for(long long i=2;i<=s2;i++){
+		if(!prime[i])continue;
+		for(long long j = i*i; j <= s2; j+=i){
+			prime[j]=false;
+		}
 		mul=i*i;
 		tt = start/mul;
 		ttt = endN/mul;
