53dadab23d522e334f754ba6285b48ca8a8ede68
jaejin <78140235+jeajin@users.noreply.github.com>
2024-09-04 00:07:14 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 16 ms, Memory: 3008 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.cc
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.cc
@@ -5,7 +5,7 @@ using namespace std;
 bool nn[max];
 
 long long start,endN;
-long long mul,temp;
+long long mul,temp,tt,ttt;
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -18,18 +18,19 @@ int main() {
     
 	for(long long i=2;i<=s2;i++){
 		mul=i*i;
-		long long tt = start/mul;
+		tt = start/mul;
+		ttt = endN/mul;
 		if(start%mul!=0)tt++;
-        for(long long j=tt;j<endN/mul+1;j++){
+        for(long long j=tt;j<=ttt;j++){
 			nn[mul*j-start]=false;
 		}
 		
 	}
 	
-	temp = 0;
-	long long count = 0;
-	while(temp<(endN-start+1)){
-		if(nn[temp++])count++;	
+	temp = -1;
+	int count = 0;
+	while(++temp<(endN-start+1)){
+		if(nn[temp])count++;	
 	}
 	printf("%d",count);
 
