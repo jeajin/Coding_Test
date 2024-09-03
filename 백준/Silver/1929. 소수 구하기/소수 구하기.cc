bfd27bba50f8a35c1a7e83490f2fc5dab4473a59
jaejin <78140235+jeajin@users.noreply.github.com>
2024-09-03 18:34:07 +0900

    [Silver III] Title: 소수 구하기, Time: 140 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -1,20 +1,48 @@
-#include <stdio.h>
+#include <iostream>
 #include <math.h>
-int main() {
-	int i,max,sum=0,s=10001,k,t;
-	scanf("%d %d",&i,&max);	
-    if(i==1)i++;
-	for(i;i<=max;i++){
-		t=1;		
-		for(k=2;k<=sqrt(i);k++){
-			if(i%k==0){
-				t=0;					
-				break;
-			}
-		}		
-		if(t){			
-		printf("%d\n",i);
-		}		
+using namespace std;
+
+int prime[1000001];
+int start,endN;
+
+bool is_prime(int num){
+	for(int i=2;i*i<=num;i++){
+		if(num%i==0){
+			return 0;
+		}
 	}
-	return 0;
+	return 1;
+}
+void set_prime_multi_zero(int num){
+	int mul=num;
+	while((mul+=num)<=endN){
+		prime[mul]=0;
+	}
+}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); 
+	cout.tie(NULL);    
+	
+	cin >> start >> endN;
+	
+	fill_n(prime, endN+1, 1); 
+	
+ 	prime[1]=0;
+ 	set_prime_multi_zero(2);
+ 	
+	for(int i=3;i<=endN;){
+		if(is_prime(i)){
+			set_prime_multi_zero(i);
+		}
+		while(prime[++i]==0){
+		}
+					
+	}
+	
+	for(int i=start;i<=endN;i++){
+		if(prime[i]){
+			printf("%d ",i);
+		}
+	}
 }
