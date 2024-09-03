718eb693dc2d3ef1a47c84adad46b5ae77ec09fa
jaejin <78140235+jeajin@users.noreply.github.com>
2024-09-04 01:42:13 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 4 ms, Memory: 3008 KB -BaekjoonHub

--- 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.cc
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.cc
@@ -9,16 +9,10 @@ int main() {
 	cin.tie(NULL); 
 	cout.tie(NULL);    	
 	cin >>  start;
-	fill_n(prime, 1000001, 1); 
     long long s2 = sqrt(start);
+	fill_n(prime, s2+1, 1); 
     long long count = 1;
-    for(long long i=2;i<=s2;i++){
-    	if(prime[i]){
-    		for(mul=i*i;mul<=s2;mul+=i){
-					prime[mul]=false;
-			}
-		}
-	}
+
 	    
 	for(long long i=2;i<=s2;i++){
 		if(prime[i]){
@@ -29,11 +23,13 @@ int main() {
 					count*=i;
 					start/=i;
 				}
+			}
+			for(mul=i*i;mul<=s2;mul+=i){
+				prime[mul]=false;
 			}			
 		}		
 	}
 	if(start!=1)count*=(start-1);
-
 	printf("%lld",count);
 
 }
