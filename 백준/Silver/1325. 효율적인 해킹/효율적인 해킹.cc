d991900ab3681109458c280e2fe641159127c0b3
jaejin <78140235+jeajin@users.noreply.github.com>
2024-09-09 14:28:14 +0900

    [Silver I] Title: 효율적인 해킹, Time: 2908 ms, Memory: 3628 KB -BaekjoonHub

---
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+#define max 10000
+vector<int> edge[max+1];
+int answer[max+1];
+int visited[max+2]={0,};
+int n,m,k,x;
+int maxNum = -321;
+
+void dfs(int index){
+	visited[index]=1;
+	for(int i=0;i<edge[index].size();i++){
+		int t = edge[index][i];
+		if(t && !visited[t]){
+			answer[t]++;
+			dfs(t);
+			maxNum = maxNum>answer[t]?maxNum:answer[t];
+		}
+	}
+}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); 
+	cout.tie(NULL);    		
+
+	cin >> n >> m ;
+	
+	for(int i=0;i<m;i++){
+		int in,out;
+		cin >> in >> out;
+		edge[in].push_back(out);
+	}
+	
+	for(int i=1;i<=n;i++){
+		fill_n(visited,n+1,0);
+		dfs(i);
+	}
+	
+	for(int i=1;i<=n;i++){
+		if(maxNum==answer[i])
+		printf("%d ",i);
+	}
+}
